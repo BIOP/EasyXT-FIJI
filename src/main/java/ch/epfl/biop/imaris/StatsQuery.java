d342a9ff2d9f18b5d91866c7dea5850883c51cd3
Olivier Burri <olivier.burri@gmail.com>
2020-09-11 13:19:49 +0200

    refactors code layout for StatsQuery and avoids creating useless columns

--- src/main/java/ch/epfl/biop/imaris/StatsQuery.java
+++ src/main/java/ch/epfl/biop/imaris/StatsQuery.java
@@ -13,9 +13,9 @@ import java.util.stream.Collectors;
 
 /**
  * This class is based on a Builder model to obtain selected statistics from an Imaris {@link cStatisticValues} object
- * It allows for selecting specific item IDs, stat names, channels and timepoints.
- * Statistics are returned in the form of a {@link ResultsTable}
- * Multiple images are not directly supported, as we have never had a use for it.
+ * It allows for selecting specific item IDs, stat names, channels and timepoints. Statistics are returned in the form
+ * of a {@link ResultsTable} Multiple images are not directly supported, as we have never had a use for it.
+ *
  * @author Olivier Burri
  * @version 0.1
  */
@@ -31,6 +31,7 @@ public class StatsQuery {
 
     /**
      * Constructor for getting selected statistics
+     *
      * @param item the Imaris object from which we want statistics (Spots, Surfaces, ...)
      * @throws Error an Imaris Error Object
      */
@@ -48,38 +49,42 @@ public class StatsQuery {
     /**
      * Allows for the selection of a specific ID. Note that IDs are not necesarily continuous nor necesarily start at 0.
      * These are the IDs as per Imaris's ID value in the GUI
+     *
      * @param id the ID to recover statistics from
      * @return
      */
     public StatsQuery selectId( final Integer id ) {
-        this.ids.add( id.longValue() );
+        this.ids.add( id.longValue( ) );
         return this;
     }
 
     /**
      * allows to set a list of IDs from which to get statistics from
+     *
      * @param ids the list of IDs to recover statistics from
      * @return
      */
     public StatsQuery selectIds( final List<Integer> ids ) {
-        this.ids.addAll( ids.stream( ).map( id -> id.longValue() ).collect( Collectors.toList( ) ) );
+        this.ids.addAll( ids.stream( ).map( id -> id.longValue( ) ).collect( Collectors.toList( ) ) );
         return this;
     }
 
     /**
-     * allows to select the name of the statistic to export. These are the same names as in the Imaris GUI
-     * **Minus** the channel or image (eg. do not enter "Intensity Sum" Ch1=1 Img=1, just "Intensity Sum")
-     * Use {@link StatsQuery#selectChannels(List)} and {@link StatsQuery#selectChannel(Integer)} to specify channels
+     * allows to select the name of the statistic to export. These are the same names as in the Imaris GUI **Minus** the
+     * channel or image (eg. do not enter "Intensity Sum" Ch1=1 Img=1, just "Intensity Sum") Use {@link
+     * StatsQuery#selectChannels(List)} and {@link StatsQuery#selectChannel(Integer)} to specify channels
+     *
      * @param name the name of the statistic to recover as it appears in the Imaris GUI.
      * @return
      */
-    public StatsQuery selectStatistic( final String name) {
+    public StatsQuery selectStatistic( final String name ) {
         this.names.add( name );
         return this;
     }
 
     /**
      * allows to set a list of IDs from which to get statistics from
+     *
      * @param names the list of statistic names to recover as they appear in the Imaris GUI
      * @return
      */
@@ -90,16 +95,18 @@ public class StatsQuery {
 
     /**
      * allows to select the timepoint of the statistics to export. 0-based
+     *
      * @param timepoint the timepoint
      * @return
      */
     public StatsQuery selectTime( final Integer timepoint ) {
-        this.timepoints.add( timepoint.toString() );
+        this.timepoints.add( timepoint.toString( ) );
         return this;
     }
 
     /**
      * allows to set a list of timepoints from which to get statistics from
+     *
      * @param timepoints
      * @return
      */
@@ -110,16 +117,18 @@ public class StatsQuery {
 
     /**
      * allows to select the channel from which to get statistics from
+     *
      * @param channel
      * @return
      */
     public StatsQuery selectChannel( final Integer channel ) {
-        this.channels.add( channel.toString() );
+        this.channels.add( channel.toString( ) );
         return this;
     }
 
     /**
      * allows to set a list of channels from which to get statistics from
+     *
      * @param channels
      * @return
      */
@@ -134,6 +143,7 @@ public class StatsQuery {
 
     /**
      * Allows appending results from a previous run
+     *
      * @param results a results table from ImageJ or from a finished StatsQuery
      * @return
      */
@@ -149,9 +159,10 @@ public class StatsQuery {
     }
 
     /**
-     * Heavy lifting function that performs the requested operation and returns a table
-     * It is rather naive. It will go through each row of the raw Imaris statistics and see if that row matches
-     * the names, channels and timepoints that were requested. If they all match
+     * Heavy lifting function that performs the requested operation and returns a table It is rather naive. It will go
+     * through each row of the raw Imaris statistics and see if that row matches the names, channels and timepoints that
+     * were requested. If they all match
+     *
      * @return
      * @throws Error
      */
@@ -195,17 +206,22 @@ public class StatsQuery {
 
             // If we get a true for all these, then we can keep the statistic
             if ( matchesName && matchesChannel && matchesID && matchesTime ) {
-                // Make a new results
-                results.incrementCounter( );
                 String columnName = stats.mNames[ i ];
                 Float value = stats.mValues[ i ];
-                results.addLabel( new File( EasyXT.getOpenImageName( ) ).getName( ) );
-                results.addValue( "Channel", this.stats.mFactors[ channelIdx ][ i ] );
-                results.addValue( "Timepoint", this.stats.mFactors[ timeIdx ][ i ] );
-                results.addValue( "ID", this.stats.mIds[ i ] );
-                results.addValue( "Category", this.stats.mFactors[ catIdx ][ i ] );
+                String cat = this.stats.mFactors[ catIdx ][ i ];
+                String channel = this.stats.mFactors[ channelIdx ][ i ];
+                String time = this.stats.mFactors[ timeIdx ][ i ];
+                long id = this.stats.mIds[ i ];
 
+                // Make a new results row
+                results.incrementCounter( );
+
+                results.addLabel( new File( EasyXT.getOpenImageName( ) ).getName( ) );
+                results.addValue( "ID", id );
                 results.addValue( columnName, value );
+                if ( !cat.equals( "" ) ) results.addValue( "Category", cat );
+                if ( !channel.equals( "" ) ) results.addValue( "Channel", channel );
+                if ( !time.equals( "" ) ) results.addValue( "Timepoint", time );
             }
         }
         return this.results;

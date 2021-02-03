a4b5c7dbca142dbe52b77405c8932dd026f9e4b8
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2021-02-03 15:21:00 +0100

    adds licenses

--- src/main/java/ch/epfl/biop/imaris/ItemQuery.java
+++ src/main/java/ch/epfl/biop/imaris/ItemQuery.java
@@ -1,3 +1,24 @@
+/*-
+ * #%L
+ * API and commands to facilitate communication between Imaris and FIJI
+ * %%
+ * Copyright (C) 2020 - 2021 ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, BioImaging And Optics Platform (BIOP)
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
 /*
  * Copyright (c) 2021 Ecole Polytechnique Fédérale de Lausanne. All rights reserved.
  * Redistribution and use in source and binary forms, with or without modification,

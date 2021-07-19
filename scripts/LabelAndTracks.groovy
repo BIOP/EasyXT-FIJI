8047fe424a5f231ce6c7ade531d79072a2a5d2d8
romainGuiet <romain.guiet@epfl.ch>
2021-07-19 15:30:58 +0200

    updates LabelAndTracks.groovy with comments , modifies loop end thanks to bug fix with createFromLabels()

--- scripts/LabelAndTracks.groovy
+++ scripts/LabelAndTracks.groovy
@@ -1,4 +1,3 @@
-package ImarisIssues
 
 import Imaris.ISurfacesPrx
 import ch.epfl.biop.imaris.EasyXT
@@ -7,7 +6,7 @@ import ij.*
 
 IJ.run("Close All", "");
 
-// Imaris comes with a couple of images, stored into user...
+// Imaris comes with a couple of images, stored into user\...
 // "celldemo.ims" , ...
 image_path = EasyXT.Samples.getImarisDemoFile("CellDevelopment.ims")
 println image_path
@@ -19,45 +18,39 @@ imp = EasyXT.Dataset.getImagePlus(EasyXT.Dataset.getCurrent())
 cal = imp.getCalibration()
 imp.show()
 
+// get the nucleus channel
 def nuc_imp = new Duplicator().run(imp, 1, 1, 1, imp.getNSlices(), 1, imp.getNFrames());
 
 nuc_imp.show()
-
-IJ.setRawThreshold(nuc_imp, 100, 255, null);
+// Threshold and "clean" the image with a median filter
+IJ.setRawThreshold(nuc_imp, 100, 255, null);
 IJ.run(nuc_imp, "Convert to Mask", "background=Dark black");
+IJ.run(nuc_imp, "Median 3D...", "x=2 y=2 z=2");
 
-
-IJ.run(nuc_imp, "Median 3D...", "x=2 y=2 z=2");
-
-
-//nFrames = imp.getNFrames()
+// Segment Nuclei using "3D Nuclei Segmentation"
 def hypStk = []
-(1..imp.getNFrames()-1).each{
-//(1..5).each{
+(1..imp.getNFrames()).each{
 	mask = new Duplicator().run(nuc_imp, 1, 1, 1, imp.getNSlices(), it, it);
 	IJ.run(mask, "3D Nuclei Segmentation (beta)", "auto_threshold=Default manual=128 separate_nuclei");
 	labels_imp = IJ.getImage()
 	t_imp = labels_imp.duplicate()
 	hypStk.add(t_imp)
 }
-
-theImp =  Concatenator.run( hypStk as ImagePlus[])
+theImp =  Concatenator.run( hypStk as ImagePlus[])
 theImp.show()
 
-
+// Create a Surfaces object from the Label image
 surf = EasyXT.Surfaces.createFromLabels(theImp)
 EasyXT.Scene.setName(surf, "ConnectedComponent")
 EasyXT.Scene.addItem(surf)
 
-
-// Track Surfaces
-
-tracked_surf = (ISurfacesPrx) EasyXT.Tracks.create(surf)
+// Track the Surfaces
+tracked_surf = (ISurfacesPrx) EasyXT.Tracks.create(surf)
         .useBrownianMotion()
         .setMaxDistance(10.0)
         .setGapSize(3)
         .build()
         .track()
 
-EasyXT.Scene.setName(tracked_surf, "Tracked_")
+EasyXT.Scene.setName(tracked_surf, "Tracked_"+EasyXT.Scene.getName(surf))
 EasyXT.Scene.addItem(tracked_surf)

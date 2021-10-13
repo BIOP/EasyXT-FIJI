dd33bca3aca11f9a613053c8db75b07f98b6e04a
romainGuiet <romain.guiet@epfl.ch>
2021-10-13 13:20:41 +0200

    homogenizes and make Spots.filter ...

--- src/main/java/ch/epfl/biop/imaris/EasyXT.java
+++ src/main/java/ch/epfl/biop/imaris/EasyXT.java
@@ -1651,57 +1651,7 @@ public class EasyXT {
          */
         public static ISurfacesPrx filter(ISurfacesPrx surface, String columnName, double minValue, double maxValue) throws Error {
 
-            ResultsTable rt = Stats.export(surface, columnName);
-
-            //String[] headers = rt.getHeadingsAsVariableNames();
-            //int indexIDColumn = Arrays.asList(headers).indexOf("ID");
-
-            Variable[] ids_vars = rt.getColumnAsVariables("ID");
-
-            double[] ids = Arrays.stream(ids_vars).map(var -> var.getValue()).mapToDouble(d -> d).toArray();
-            // current @EasyXT.Stats.export() table are string (needs >= 1.53j ? , to getColumnAsStrings() )
-            // and will also need parseDouble
-            //String[] values;
-            String[] values = Arrays.stream(rt.getColumnAsVariables(columnName)).map(var ->var.toString()).toArray(String[]::new);
-
-            // Here we'll filtered the ids if they pass the test :  minValue < value < maxValue
-            // use List to add item
-            List<Integer> filteredIdsList = new ArrayList<>();
-            for (int i = 0; i < ids.length; i++) {
-                if ((Double.parseDouble(values[i]) >= minValue) && (Double.parseDouble(values[i]) <= maxValue)) {
-                    filteredIdsList.add((int) ids[i]);
-                }
-            }
-
-            // CopySurfaces requires a int[] so need to convert the List
-            int[] filteredIds = filteredIdsList.stream().mapToInt(i -> i).toArray();
-            ISurfacesPrx filteredSurface = surface.CopySurfaces(filteredIds);
-
-            return filteredSurface;
-
-            // With ij version >= 1.53m
-            /*
-            double[] ids = rt.getColumn("ID");
-            // current @EasyXT.Stats.export() table are string (needs >= 1.53j ? , to getColumnAsStrings() )
-            // and will also need parseDouble
-            String[] values;
-            values = rt.getColumnAsStrings(columnName);
-
-            // Here we'll filtered the ids if they pass the test :  minValue < value < maxValue
-            // use List to add item
-            List<Integer> filteredIdsList = new ArrayList<>();
-            for (int i = 0; i < ids.length; i++) {
-                if ((Double.parseDouble(values[i]) >= minValue) && (Double.parseDouble(values[i]) <= maxValue)) {
-                    filteredIdsList.add((int) ids[i]);
-                }
-            }
-
-            // CopySurfaces requires a int[] so need to convert the List
-            int[] filteredIds = filteredIdsList.stream().mapToInt(i -> i).toArray();
-            ISurfacesPrx filteredSurface = surface.CopySurfaces(filteredIds);
-
-            return filteredSurface;
-            */
+           return (ISurfacesPrx) Utils.filter( surface,  columnName,  minValue,  maxValue);
 
         }
 
@@ -1844,6 +1794,53 @@ public class EasyXT {
         public static ISpotsPrx find(String name, IDataContainerPrx parent) throws Error {
             return Scene.findSpots(name, parent);
         }
+
+        /**
+         * Returns an Imaris surface filtered with a test minValue &lt; value &gt; maxValue for a defined columnName
+         *
+         * @param spots    the surface to filter
+         * @param columnName ColumnName as displayed in ImageJ Results Table you got from @EasyXT.Stats.export()
+         * @param minValue   the minimum value
+         * @param maxValue   the maximum value
+         * @return filteredSurface the filtered surface
+         * @throws Error an Imaris Error
+         */
+        public static ISpotsPrx filter(ISpotsPrx spots, String columnName, double minValue, double maxValue) throws Error {
+
+            return (ISpotsPrx) Utils.filter( spots,  columnName,  minValue,  maxValue);
+
+        }
+
+        /**
+         * Returns an Imaris surface filtered with a test minValue &lt; value for a defined columnName
+         *
+         * @param spots    the surface to filter
+         * @param columnName ColumnName as displayed in ImageJ Results Table you got from @EasyXT.Stats.export()
+         * @param minValue   the minimum value
+         * @return filteredSurface the filtered surface
+         * @throws Error an Imaris Error
+         */
+        public static ISpotsPrx filterAbove(ISpotsPrx spots, String columnName, double minValue) throws Error {
+
+            return EasyXT.Spots.filter(spots, columnName, minValue, Double.MAX_VALUE);
+
+        }
+
+        /**
+         * Returns an Imaris surface filtered with a test value &gt; maxValue for a defined columnName
+         *
+         * @param spots    the surface to filter
+         * @param columnName ColumnName as displayed in ImageJ Results Table you got from @EasyXT.Stats.export()
+         * @param maxValue   the minimum value
+         * @return filteredSurface the filtered surface
+         * @throws Error an Imaris Error
+         */
+        public static ISpotsPrx filterBelow(ISpotsPrx spots, String columnName, double maxValue) throws Error {
+
+            return EasyXT.Spots.filter(spots, columnName, -1 * Double.MAX_VALUE, maxValue);
+
+        }
+
 
         /**
          * returns the first Spots object in the main surpass scene with the given name
@@ -2196,83 +2193,41 @@ public class EasyXT {
             IFactoryPrx factory = EasyXT.Utils.getImarisApp().GetFactory();
             IDataItemPrx aItemFiltered = null;
 
+            // current @EasyXT.Stats.export() table are string
+            // Issue with using imagej= 1.53j ? , to getColumnAsStrings() )
+            // workaround use Variable[]
             ResultsTable rt = Stats.export(aItem, columnName);
 
-            //String[] headers = rt.getHeadingsAsVariableNames();
-            //int indexIDColumn = Arrays.asList(headers).indexOf("ID");
-
-            Variable[] ids_vars = rt.getColumnAsVariables("ID");
-
-            double[] ids = Arrays.stream(ids_vars).map(var -> var.getValue()).mapToDouble(d -> d).toArray();
-            // current @EasyXT.Stats.export() table are string (needs >= 1.53j ? , to getColumnAsStrings() )
-            // and will also need parseDouble
-            //String[] values;
-            String[] values = Arrays.stream(rt.getColumnAsVariables(columnName)).map(var -> var.toString()).toArray(String[]::new);
+            double[] ids = Arrays.stream(rt.getColumnAsVariables("ID")).map(var -> var.getValue()).mapToDouble(d -> d).toArray();
+            double[] values = Arrays.stream(rt.getColumnAsVariables(columnName)).map(var -> var.getValue()).mapToDouble(d -> d).toArray();
 
             // Here we'll filtered the ids if they pass the test :  minValue < value < maxValue
             // use List to add item
             List<Integer> filteredIdsList = new ArrayList<>();
             for (int i = 0; i < ids.length; i++) {
-                if ((Double.parseDouble(values[i]) >= minValue) && (Double.parseDouble(values[i]) <= maxValue)) {
+                if ( (values[i] >= minValue) && (values[i] <= maxValue)) {
                     filteredIdsList.add((int) ids[i]);
                 }
             }
-
 
             // spots or surfaces ?
             if (factory.IsSpots(aItem)) {
                 // copySpots requires a long[] so need to convert the List
                 long[] filteredIds = filteredIdsList.stream().mapToLong(l -> l).toArray();
                 ISpotsPrx spots_tofilter = (ISpotsPrx) EasyXT.Utils.castToType(aItem);
-                ISpotsPrx spots_filtered = null;
+                ISpotsPrx spots_filtered ;
                 spots_filtered = copySpots(spots_tofilter, filteredIds);
                 aItemFiltered = spots_filtered;
             } else if (factory.IsSurfaces(aItem)) {
                 // CopySurfaces requires a int[] so need to convert the List
                 int[] filteredIds = filteredIdsList.stream().mapToInt(i -> i).toArray();
                 ISurfacesPrx surfacesToFilter = (ISurfacesPrx) EasyXT.Utils.castToType(aItem);
-                ISurfacesPrx surfacesFiltered = null;
+                ISurfacesPrx surfacesFiltered ;
                 surfacesFiltered = surfacesToFilter.CopySurfaces(filteredIds);
                 aItemFiltered = surfacesFiltered;
             }
 
             return aItemFiltered;
-
-            /*
-            double[] ids = rt.getColumn("ID");
-            // current @EasyXT.Stats.export() table are string (needs >= 1.53j ? , to getColumnAsStrings() )
-            // and will also need parseDouble
-            String[] values;
-            values = rt.getColumnAsStrings(columnName);
-
-            // Here we'll filtered the ids if they pass the test :  minValue < value < maxValue
-            // use List to add item
-            List<Integer> filteredIdsList = new ArrayList<>();
-            for (int i = 0; i < ids.length; i++) {
-                if ((Double.parseDouble(values[i]) >= minValue) && (Double.parseDouble(values[i]) <= maxValue)) {
-                    filteredIdsList.add((int) ids[i]);
-                }
-            }
-
-
-            // spots or surfaces ?
-            if (factory.IsSpots(aItem)) {
-                // copySpots requires a long[] so need to convert the List
-                long[] filteredIds = filteredIdsList.stream().mapToLong(l -> l).toArray();
-                ISpotsPrx spots_tofilter = (ISpotsPrx) EasyXT.Utils.castToType(aItem);
-                ISpotsPrx spots_filtered = null;
-                spots_filtered = copySpots(spots_tofilter, filteredIds);
-                aItemFiltered = spots_filtered;
-            } else if (factory.IsSurfaces(aItem)) {
-                // CopySurfaces requires a int[] so need to convert the List
-                int[] filteredIds = filteredIdsList.stream().mapToInt(i -> i).toArray();
-                ISurfacesPrx surfacesToFilter = (ISurfacesPrx) EasyXT.Utils.castToType(aItem);
-                ISurfacesPrx surfacesFiltered = null;
-                surfacesFiltered = surfacesToFilter.CopySurfaces(filteredIds);
-                aItemFiltered = surfacesFiltered;
-            }
-
-            return aItemFiltered;*/
 
         }
 

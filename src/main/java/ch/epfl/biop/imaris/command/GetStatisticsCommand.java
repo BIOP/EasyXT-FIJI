6e23881baa323ecb90107784bfe0edd7458ee032
oburri <olivier.burri@gmail.com>
2023-04-17 10:56:37 +0200

    arbitrary object statistics command

---
+++ src/main/java/ch/epfl/biop/imaris/command/GetStatisticsCommand.java
@@ -0,0 +1,55 @@
+/*-
+ * #%L
+ * API and commands to facilitate communication between Imaris and FIJI
+ * %%
+ * Copyright (C) 2020 - 2023 ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, BioImaging And Optics Platform (BIOP)
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+package ch.epfl.biop.imaris.command;
+
+import Imaris.Error;
+import Imaris.IDataItemPrx;
+import Imaris.ISpotsPrx;
+import ch.epfl.biop.imaris.EasyXT;
+import ij.measure.ResultsTable;
+import org.scijava.ItemIO;
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+@Plugin(type = Command.class, menuPath = "Plugins>BIOP>EasyXT>Spots>Get Object Statistics")
+public class GetStatisticsCommand implements Command {
+
+    @Parameter
+    String objectName;
+    @Parameter(type = ItemIO.OUTPUT)
+    ResultsTable results;
+
+    @Override
+    public void run() {
+        // Gets an existing surface
+        try {
+            IDataItemPrx objectprx = EasyXT.Scene.findItem(objectName);
+            results = EasyXT.Stats.export(objectprx);
+            results.show(objectName +" - all statistics");
+        } catch (Error error) {
+            error.printStackTrace();
+        }
+
+
+    }
+}

44d497d68a5eb83da7b8484e2c94b63aea861ddf
oburri <olivier.burri@gmail.com>
2024-05-30 14:37:15 +0200

    updates license files

--- src/main/java/ch/epfl/biop/imaris/demo/MakeSurfaceFromMaskDemo.java
+++ src/main/java/ch/epfl/biop/imaris/demo/MakeSurfaceFromMaskDemo.java
@@ -2,7 +2,7 @@
  * #%L
  * API and commands to facilitate communication between Imaris and FIJI
  * %%
- * Copyright (C) 2020 - 2023 ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, BioImaging And Optics Platform (BIOP)
+ * Copyright (C) 2020 - 2024 ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, BioImaging And Optics Platform (BIOP)
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
